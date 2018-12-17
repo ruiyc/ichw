c42331a0b86a659263ac0f918aa4b52df281c430
ruiyc <43408929+ruiyc@users.noreply.github.com>
2018-12-17 22:32:51 +0800

    Update tile.py

--- pyassign3/tile.py
+++ pyassign3/tile.py
@@ -5,74 +5,98 @@ def pu(m,n,a,b,c,oa,ans):
     oa为其中一个答案（one answer）
     ans为最终答案"""
     if (m*n)%(a*b)!=0:
-        return "no"
-    else:
-        h=0
-        """这是个布尔值"""
-        for i in range(m*n):
-            if c[i]==0 and h==0:
-                h=1
-                """改变布尔值，使得只对第一块找到的没铺上的砖进行操作"""
-                x=0
-                """这是个计数器"""
-                for w in range(b):
-                    for q in range(i+w*m,i+w*m+a):
-                        if q<m*n:
-                            if c[q]==0:
-                                x+=1
-                                """横着铺，判断是不是一块砖所覆盖的区域都还没被铺上"""
-                e=[]
-                """每个e都是一块砖"""
-                if i+(b-1)*m+a-1<=m*n-1 and m-i%m>=a and x==a*b:
-                    """判断这块砖会不会在下侧及右侧超出墙面，以及是不是满足上面的判断"""
-                    for k in range(b):
-                        for j in range(i+k*m,i+k*m+a):
-                            c[j]=1
-                            e.append(j)
-                            """铺砖"""
-                    oa.append(e)
-                    """记录下每块砖"""
-                    if 0 not in c:                      
-                        ans.append(oa.copy())
-                    else:
-                        pu(m,n,a,b,c,oa,ans)
-                    for k in range(b):
-                        for j in range(i+k*m,i+k*m+a):
-                            c[j]=0
-                    del oa[-1]
-                    """拆砖"""
-                y=0
-                """开始考虑竖着铺，一切同上"""
-                for w in range(a): 
-                    for q in range(i+w*m,i+w*m+b):
-                        if q<m*n:
-                            if c[q]==0:
-                                y+=1
-                e=[]
-                if i+(a-1)*m+b-1<=m*n-1 and m-i%m>=b and y==a*b:
-                    for k in range(a): 
-                        for j in range(i+k*m,i+k*m+b):
-                            c[j]=1
-                            e.append(j)
-                    oa.append(e)
-                    if 0 not in c:
-                        ans.append(oa.copy())
-                    else:
-                        pu(m,n,a,b,c,oa,ans)
-                    for k in range(a):
-                        for j in range(i+k*m,i+k*m+b):
-                            c[j]=0
-                    del oa[-1]
         return ans
+    else:
+        if a==b:
+            """单独判断正方形砖块"""
+            if m%a==0 and n%a==0:
+                """判断墙的长和宽能否整除砖的边长"""
+                for i in range(m*n):
+                    if c[i]==0:
+                        e=[]
+                        """每个e都是一块砖"""
+                        if i+(b-1)*m+a-1<=m*n-1 and m-i%m>=a:
+                            """判断这块砖会不会在下侧及右侧超出墙面"""
+                            for k in range(b):
+                                for j in range(i+k*m,i+k*m+a):
+                                    c[j]=1
+                                    e.append(j)
+                                    """铺砖"""
+                            oa.append(e)
+                        if len(ans)==0:
+                            ans.append(oa)
+            else:
+                return ans
+        else:
+            h=0
+            """这是个布尔值"""
+            for i in range(m*n):
+                if c[i]==0 and h==0:
+                    h=1
+                    """改变布尔值，使得只对第一块找到的没铺上的砖进行操作"""
+                    x=0
+                    """这是个计数器"""
+                    for w in range(b):
+                        for q in range(i+w*m,i+w*m+a):
+                            if q<m*n:
+                                if c[q]==0:
+                                    x+=1
+                                    """横着铺，判断是不是一块砖所覆盖的区域都还没被铺上"""
+                    e=[]
+                    """每个e都是一块砖"""
+                    if i+(b-1)*m+a-1<=m*n-1 and m-i%m>=a and x==a*b:
+                        """判断这块砖会不会在下侧及右侧超出墙面，以及是不是满足上面的判断"""
+                        for k in range(b):
+                            for j in range(i+k*m,i+k*m+a):
+                                c[j]=1
+                                e.append(j)
+                                """铺砖"""
+                        oa.append(e)
+                        """记录下每块砖"""
+                        if 0 not in c:                      
+                            ans.append(oa.copy())
+                        else:
+                            pu(m,n,a,b,c,oa,ans)
+                        for k in range(b):
+                            for j in range(i+k*m,i+k*m+a):
+                                c[j]=0
+                        del oa[-1]
+                        """拆砖"""
+                    y=0
+                    """开始考虑竖着铺，一切同上"""
+                    for w in range(a): 
+                        for q in range(i+w*m,i+w*m+b):
+                            if q<m*n:
+                                if c[q]==0:
+                                    y+=1
+                    e=[]
+                    if i+(a-1)*m+b-1<=m*n-1 and m-i%m>=b and y==a*b:
+                        for k in range(a): 
+                            for j in range(i+k*m,i+k*m+b):
+                                c[j]=1
+                                e.append(j)
+                        oa.append(e)
+                        if 0 not in c:
+                            ans.append(oa.copy())
+                        else:
+                            pu(m,n,a,b,c,oa,ans)
+                        for k in range(a):
+                            for j in range(i+k*m,i+k*m+b):
+                                c[j]=0
+                        del oa[-1]
+            return ans
 def hua(m,n,a,b,ans):
     import turtle
     """开始生成墙面"""
     p=turtle.Pen()
     p.color("blue")
     turtle.hideturtle()
+    p.penup()
+    p.goto(-50,50)
+    p.pendown()
     p.forward(m*30)
     p.penup()
-    p.goto(0,-30)
+    p.goto(-50,20)
     p.pendown()
     """画第一条横线"""
     for i in range(m*n):
@@ -83,17 +107,17 @@ def hua(m,n,a,b,ans):
         if (i+1)%m==0:
             """判断画完了一行，转去下一行"""
             p.penup()
-            p.goto(0,-30*(((i+1)//m)+1))
+            p.goto(-50,-30*(((i+1)//m)+1)+50)
             p.pendown()
     p.penup()
-    p.goto(0,0)
+    p.goto(-50,50)
     p.pendown()
     p.right(90)
     """开始画竖线"""
     for i in range(m+1):
         p.forward(30*n)
         p.penup()
-        p.goto(30*(i+1),0)
+        p.goto(30*(i+1)-50,50)
         p.pendown()
     """开始画方案"""
     p.color("black")
@@ -103,7 +127,7 @@ def hua(m,n,a,b,ans):
         if (i[-1]-i[0])==(m*(b-1)+(a-1)):
             """判断这块砖是横着铺的"""
             p.penup()
-            p.goto((i[0]%m)*30,-(i[0]//m*30))
+            p.goto((i[0]%m)*30-50,-(i[0]//m*30)+50)
             p.pendown()
             p.forward(30*a)
             p.right(90)
@@ -116,7 +140,7 @@ def hua(m,n,a,b,ans):
         else:
             """竖着的砖"""
             p.penup()
-            p.goto((i[0]%m)*30,-(i[0]//m*30))
+            p.goto((i[0]%m)*30-50,-(i[0]//m*30)+50)
             p.pendown()
             p.forward(30*b)
             p.right(90)
@@ -135,6 +159,7 @@ def main():
     print("\n")
     c=[0]*m*n
     ans=[]
+    
     pu(m,n,a,b,c,[],ans)
     print("一共有%d种方案"%(len(ans)))
     print("\n")
